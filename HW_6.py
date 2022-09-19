0f031fda6c819356b64781c674461f43f42577dc
Harry91T <igortroshyn@gmail.com>
2022-09-19 14:07:31 +0200

    HW_less7

---
+++ HW_6.py
@@ -0,0 +1,7 @@
+my_list = [23, 108, 678, 678658, 3223, 99, ]
+    for values > 100
+    print(int(values))
+
+
+
+
--- HW_less_5.py
+++ HW_less_5.py
@@ -1,5 +1,117 @@
-my_string_1 = "0123456789"
-my_string_2 = "0123456789"
-for symb_1 in my_string_1:
-    for symb_2 in my_string_2:
-     print(int(symb_1+symb_2))
+values = (1, 2, 3, 4, 5)
+
+values = list(values)
+
+print(type(values))
+
+
+values = (1, 2, 3, 4, 5)
+
+result = []
+
+for value in values:
+
+.... result.append(value)
+
+print(result)
+
+
+values = (1, 2, 3, 4, 5)
+
+result = []
+
+for value in values[::-1]:
+
+    result.append(value)
+
+print(result)
+
+
+values = [1, 2, 3, 4, 5]
+
+new_values = values + values[::-1]
+
+print(new_values)
+
+
+
+
+
+values = [1, 2, 3, 4, 5]
+
+new_values = values[::-1] * 2
+
+print(new_values)
+
+
+values = [1, 2, 3, 4, 5]
+
+new_values = values
+
+new_values.append(6)
+
+print(values)
+
+
+
+values = [1, 2, 3, 4, 5]
+
+new_values = values.copy()
+
+new_values.append(6)
+
+print(values)
+
+my_list = list("qwerty")
+
+my_str = " ".join(my_list)
+
+print(my_str)
+
+
+my_list = list("qwerty")
+
+my_str = "_".join(my_list[::-1])
+
+print(my_str)
+
+
+my_list = list("qwerty")
+
+my_str = "".join(my_list[::-1])
+
+print(my_str[::-1])
+
+
+
+
+
+values = [1, 2, 3, 4, 5]
+
+new_values = values
+
+new_values.append(6)
+
+print(values)
+values = [1, 2, 3, 4, 5]
+
+new_values = values.copy()
+
+new_values.append(6)
+
+print(values)
+my_list = list("qwerty")
+
+my_str = " ".join(my_list)
+
+print(my_str)
+my_list = list("qwerty")
+
+my_str = "_".join(my_list[::-1])
+
+print(my_str)
+my_list = list("qwerty")
+
+my_str = "".join(my_list[::-1])
+
+print(my_str[::-1])
