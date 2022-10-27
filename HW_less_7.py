75aab770fd209fddf4c01009888125ac0fa9f083
Harry91T <igortroshyn@gmail.com>
2022-10-27 14:02:53 +0200

    HW_less8

---
+++ HW_less_7.py
@@ -0,0 +1,18 @@
+1###(уверен,что можно проще)
+
+my_value = 10054306540
+my_symbol = 0
+new_value = str(my_value)
+new_symbol = str(my_symbol)
+amount = new_value.count(new_symbol)
+
+
+2###
+
+my_symbol = 100500
+new_symbol = len(str(my_symbol))
+# reversed_symbol = new_symbol[::-1]
+print(new_symbol)
+
+
+
